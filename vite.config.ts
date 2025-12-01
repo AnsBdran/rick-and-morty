70c042159a16439d38a4ea60d58cad3d53b54e7e
Anas Badran <ansaccts@gmail.com>
2025-12-01 13:52:38 +0200

    feat(config): Configure API proxy in Vite development server

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,30 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    tanstackRouter({
-      target: "react",
-      autoCodeSplitting: true,
-    }),
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return {
+    plugins: [
+      tanstackRouter({
+        target: "react",
+        autoCodeSplitting: true,
+      }),
+      react({
+        babel: {
+          plugins: [["babel-plugin-react-compiler"]],
+        },
+      }),
+    ],
+    server: {
+      proxy: {
+        "^/api/.*": {
+          target: process.env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
-    }),
-  ],
+    },
+  };
 });
