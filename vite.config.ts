2d08c6cd09d0b04209c41580cb2ac03bf159b3c8
Anas Badran <ansaccts@gmail.com>
2025-12-01 14:50:26 +0200

    chore: minor config fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -22,7 +22,7 @@ export default defineConfig(({ mode }) => {
     ],
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "src"),
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
       },
     },
     server: {
