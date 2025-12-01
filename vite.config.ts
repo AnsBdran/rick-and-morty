027a823707160a2ee4e08b7a3b1dc3b1e0db29ac
Anas Badran <ansaccts@gmail.com>
2025-12-01 14:00:08 +0200

    chore: configure `@/` as a path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -16,7 +18,13 @@ export default defineConfig(({ mode }) => {
           plugins: [["babel-plugin-react-compiler"]],
         },
       }),
+      tailwindcss(),
     ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
     server: {
       proxy: {
         "^/api/.*": {
