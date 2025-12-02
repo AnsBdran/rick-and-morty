b3d6e0d10206a9384b9d0346f87fcd56cc4eda6e
Anas Badran <ansaccts@gmail.com>
2025-12-02 14:17:43 +0200

    feat: add api queries

---
+++ src/utils/queries.ts
@@ -0,0 +1,27 @@
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+console.log("API_BASE_URL", API_BASE_URL);
+export const getCharacters = async (query?: string) => {
+  const response = await fetch(
+    `${API_BASE_URL}/character` + (query ? `?name=${query}` : "")
+  );
+  console.log("in query", API_BASE_URL);
+  return await response.json();
+};
+
+export const getCharacterById = async (id: string) => {
+  const response = await fetch(`${API_BASE_URL}/character/${id}`);
+  return response.json();
+};
+
+export const getCharacterByName = async (name: string) => {
+  const response = await fetch(`${API_BASE_URL}/character/?name=${name}`);
+  return response.json();
+};
+
+export const getEpisodesByIds = async (ids: number[]) => {
+  if (ids.length === 0) return [];
+  const response = await fetch(`${API_BASE_URL}/episode/${ids.join(",")}`);
+  const data = await response.json();
+  // API returns single object if one id, array if multiple
+  return Array.isArray(data) ? data : [data];
+};
