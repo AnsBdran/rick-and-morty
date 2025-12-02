5eb2e1c1312f9a625b8ea48dd3069b11b28beda5
Anas Badran <ansaccts@gmail.com>
2025-12-02 14:16:24 +0200

    feat: add debounce utility

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,19 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const debounce = <T extends unknown[]>(
+  callback: (...args: T) => void,
+  wait: number
+) => {
+  let timeoutId: number | undefined;
+  return (...args: T) => {
+    window.clearTimeout(timeoutId);
+    timeoutId = window.setTimeout(() => {
+      callback(...args);
+    }, wait);
+  };
+};
