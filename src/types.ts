a7bc091d625e7d7772719692d7fb29e63b602d1f
Anas Badran <ansaccts@gmail.com>
2025-12-02 14:15:18 +0200

    feat: add api data types

---
+++ src/types.ts
@@ -0,0 +1,29 @@
+interface LocationInfo {
+  name: string;
+  url: string;
+}
+
+export interface Character {
+  id: number;
+  name: string;
+  status: "Alive" | "Dead" | "unknown";
+  species: string;
+  type: string;
+  gender: "Female" | "Male" | "Genderless" | "unknown";
+  origin: LocationInfo;
+  location: LocationInfo;
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+}
+
+export interface Episode {
+  id: number;
+  name: string;
+  air_date: string;
+  episode: string;
+  characters: string[];
+  url: string;
+  created: string;
+}
