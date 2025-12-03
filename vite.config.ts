33eadd24ab3e5130286ba984e783733b5632caa0
Anas Badran <ansaccts@gmail.com>
2025-12-03 09:15:14 +0200

    chore: lint

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath, URL } from "node:url";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+export default defineConfig(() => {
   return {
     plugins: [
       tanstackRouter({
@@ -27,10 +26,8 @@ export default defineConfig(({ mode }) => {
     },
     server: {
       proxy: {
-        "^/api/.*": {
+        "/api": {
           target: process.env.VITE_API_BASE_URL,
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ""),
         },
       },
     },
